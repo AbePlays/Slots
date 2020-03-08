fab1d6bb398b68e512a166b66e77cd0f358c51c0
Abe <abhi.rawat456@gmail.com>
2020-03-08 13:50:35 +0530

    added top and middle compartments

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -11,14 +11,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            //Background
             Rectangle()
                 .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
                 .edgesIgnoringSafeArea(.all)
             
             Rectangle()
-            .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
                 .rotationEffect(Angle(degrees: 45))
                 .edgesIgnoringSafeArea(.all)
+            
+            //Foreground
+            VStack {
+                //Title
+                HStack {
+                    Image(systemName : "star.fill").foregroundColor(.yellow)
+                    Text("SwiftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    Image(systemName : "star.fill").foregroundColor(.yellow)
+                }.scaleEffect(2)
+                
+                //Credits
+                Text("Credits : 1000")
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                
+                //Cards
+                HStack {
+                    Spacer()
+                    Image("apple")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Image("apple")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Image("apple")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Spacer()
+                }
+            }
         }
     }
 }
