cc94e6f1ed7e48fa502e79ed52f2108ac4857f6c
Abe <abhi.rawat456@gmail.com>
2020-03-08 13:38:40 +0530

    added background views

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -14,6 +14,11 @@ struct ContentView: View {
             Rectangle()
                 .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
                 .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+            .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
